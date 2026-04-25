c1e8f46aca846da88a1b701389b94c4d7b42ca61
PrashanthTiwari2905 <tiwariprashanth.2905@gmail.com>
2026-04-25 20:11:23 +0530

    Fix build: Switch to relative paths for database connectivity

--- real_estate_ai/frontend/app/api/auth/[...nextauth]/route.js
+++ real_estate_ai/frontend/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import { connectDB, User } from "@/lib/mongodb";
+import { connectDB, User } from "../../lib/mongodb";
 
 export const authOptions = {
   providers: [
